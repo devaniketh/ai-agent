3cc36b29f30b825ded1ac6a04f9d63a0741955cf
Aniketh <techrunstheworld@gmail.com>
2025-02-09 19:23:45 +0530

    Removed .env and added to .gitignore

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import { log } from 'node:console';
 import { GoogleGenerativeAI } from "@google/generative-ai";
 import readlinkSync from 'readline-sync';
 
-const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
+const genAI = new GoogleGenerativeAI()
 const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash" });
 
 async function getAllTodos() {
